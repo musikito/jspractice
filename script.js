4c61aa5f1d918afecace036c2e90bdadd1b9746d
Jason Wu <lujason2015@gmail.com>
2019-04-11 16:57:48 +0000

    edit comment

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const numbers = [67, 80, 65, 79, 86, 66, 75, 71, 81, 69, 90, 79, 68, 65, 73, 88];
 
 // 1. Convert numbers to Alphabet according to Ascii table using the fromCharCode method
-// Using the forEach method:
 // Using a for loop:
+// Using the forEach method:
 // Using the map method:
 
 // 2. Display numbers below showing only numbers that are greater than 72 and less then 88 (inclusive) using the filter method
