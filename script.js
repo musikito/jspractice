b228a6f5b7114197c13ad432e3944ceb46f4f8e7
Jason Wu <lujason2015@gmail.com>
2019-04-11 16:26:51 +0000

    update 2

---
+++ script.js
@@ -0,0 +1,12 @@
+const numbers = [67, 80, 65, 79, 86, 66, 75, 71, 81, 69, 90, 79, 68, 65, 73, 88];
+
+// 1. Convert numbers to Alphabet in order (e.g. 1 becomes A, 2 becomes B and so on)
+// Using the forEach method:
+// Using a for loop:
+// Using the map method:
+
+// 2. Display numbers below showing only numbers that are greater than 72 and less then 88 (inclusive) using the filter method
+// Answer:
+
+// 3. Display the product of all numbers using reduce
+// Answer:
