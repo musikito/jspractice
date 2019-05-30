abe858a682b55137a70720617bda98b7ae39c174
jose marte <tumusicalatina@gmail.com>
2019-05-30 12:12:54 -0400

    Added join()

--- script.js
+++ script.js
@@ -21,24 +21,25 @@ const numbers = [
 // Using a for loop:
 for (let index = 0; index < numbers.length; index++) {
   const res = String.fromCharCode(numbers[index]);
-  //document.getElementById("forLoop").innerHTML += res;
+  document.getElementById("forLoop").innerHTML += res + " ";
   console.log("for loop", res);
 }
 // Using the forEach method:
 numbers.forEach(element => {
   const res = String.fromCharCode(element);
-  document.getElementById("forEach").innerHTML += res;
+  document.getElementById("forEach").innerHTML += res + " ";
   console.log("forEach", res);
 });
 // Using the map method:
 const resMap = numbers.map(item => String.fromCharCode(item));
-document.getElementById("map").innerHTML = resMap;
+//use join to remove the commas, or add them
+document.getElementById("map").innerHTML = resMap.join(" ");
 console.log("map", resMap);
 
 // 2. Display numbers below showing only numbers that are greater than 72 and less then 88 (inclusive) using the filter method
 // Answer:
 const resFilter = numbers.filter(number => number > 72 && number <= 88);
-document.getElementById("filter").innerHTML = resFilter;
+document.getElementById("filter").innerHTML = resFilter.join(" ");
 console.log("filter", resFilter);
 // 3. Display the product of all numbers using reduce
 // Answer:
